b9b47c769dc82bddf0e54fdfdde675113bf2e7c2
Daniela <yddiaz@ucundinamarca.edu.co>
2020-08-27 19:57:37 -0500

    actualizacion uno

---
+++ dados.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class Dados {
+    public static int tirarDado(int rango) {
+        double resultado;
+        resultado=Math.random()*rango; // Se le suma 1 si el valor del 0 es 10. 
+        return (int)resultado;
+    }
+    
+    public static ArrayList tirarDados(int numero, int rango) {
+        ArrayList resultados=new ArrayList();
+        for (int i=0; i<numero; i++) {
+            resultados.add(tirarDado(rango));
+        }
+        return resultados;
+    }
+}
